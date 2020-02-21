74d1636e79b37145546587c7b2f70dfbe03b1826
ko040123 <49421104+ko040123@users.noreply.github.com>
2020-02-22 02:03:09 +0900

    Lab01.c added
    Lab01.c file was added in order to translate into cpp code

---
+++ Lab01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define N 40
+void sum(int d[], int n, int* p){
+  *p = 0;
+  for(int i = 0 ; i < n ; ++i) *p = *p + d[i];
+}
+
+int main(){
+  int total = 0;
+  int data[N];
+  for(int i = 0; i < N; ++i) data[i] = i;
+  sum(data, N, &total);
+  printf("total is %d\n", total);
+  return 0;
+}
